a59142b40aa6a378f4086a4d2a8f0472f8a5686f
Muharrem <ozcankocabiyik07@gmail.com>
2024-11-15 14:55:31 +0300

    1.5
    New update for singleton pattern -> eager initialization.

--- DesignPatterns/SingletonPatterns/Singleton_eager_initialization/Singleton.java
+++ DesignPatterns/SingletonPatterns/Singleton_eager_initialization/Singleton.java
@@ -2,8 +2,8 @@ package Singleton_eager_initialization;
 
 public class Singleton {
 
-    // the private reference to the one and only instance
-    private static Singleton uniqueInstance = null;
+    // the private reference to the one and only instance (eager initialization)
+    private static final Singleton uniqueInstance = new Singleton();
 
     // an instance attribute
     private int data = 0;
@@ -13,17 +13,14 @@ public class Singleton {
      * Note that it is private!
      * No client can instantiate a Singleton object!
      */
-
     private Singleton() {}
 
-    public static Singleton getInstance()
-    {
-        if (uniqueInstance == null)
-            uniqueInstance = new Singleton();
-
+    // Return the one and only instance
+    public static Singleton getInstance() {
         return uniqueInstance;
     }
 
+    // Getter and Setter for the data attribute
     public void setData(int myData) {
         data = myData;
     }
